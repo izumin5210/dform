bdef8a2f2b567958f681ec1a691655af95fcf45b
izumin5210 <m@izum.in>
2018-01-10 09:26:10 +0900

    Display schema string temporary

--- cmd/export.go
+++ cmd/export.go
@@ -1,7 +1,14 @@
 package cmd
 
 import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/dgraph-io/dgraph/client"
+	"github.com/dgraph-io/dgraph/protos/api"
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
 )
 
 func (r *root) newExportCommand() *cobra.Command {
@@ -10,6 +17,29 @@ func (r *root) newExportCommand() *cobra.Command {
 		Short: "Export schema information",
 		Long:  "Export schema information",
 		Run: func(*cobra.Command, []string) {
+			d, err := grpc.Dial(r.getDgraphURL(), grpc.WithInsecure())
+			if err != nil {
+				log.Fatalln(fmt.Errorf("failed to connect with Dgraph server: %v", err))
+			}
+			fmt.Println(r.getDgraphURL())
+			c := client.NewDgraphClient(api.NewDgraphClient(d))
+			ctx := context.Background()
+			txn := c.NewTxn()
+			defer txn.Discard(ctx)
+			q := `
+					schema {
+						type
+						index
+						reverse
+						tokenizer
+					}
+			`
+			resp, err := txn.Query(ctx, q)
+			if err != nil {
+				log.Fatalln(fmt.Errorf("failed to query: %v", err))
+			}
+			fmt.Println(resp.GetJson())
+			fmt.Println(resp.GetSchema())
 		},
 	}
 }
