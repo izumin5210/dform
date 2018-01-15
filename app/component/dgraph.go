cba2156336a1bc50dc1aedca7167c3f500c7b0a4
izumin5210 <m@izum.in>
2018-01-15 18:21:17 +0900

    Set zap logger to grpc conn

--- app/component/dgraph.go
+++ app/component/dgraph.go
@@ -1,10 +1,18 @@
 package component
 
 import (
+	"context"
+
+	"github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	"go.uber.org/zap/zapcore"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+
 	"github.com/izumin5210/dform/app/system"
 	"github.com/izumin5210/dform/domain/schema"
 	"github.com/izumin5210/dform/infra/repo"
-	"google.golang.org/grpc"
+	"github.com/izumin5210/dform/util/log"
 )
 
 // Dgraph containes dependencies for accessing to Dgraph.
@@ -40,10 +48,43 @@ func (d *dgraph) getConn() (*grpc.ClientConn, error) {
 	if d.conn != nil {
 		return d.conn, nil
 	}
-	conn, err := grpc.Dial(d.config.GetDgraphURL(), grpc.WithInsecure())
+	conn, err := grpc.Dial(d.config.GetDgraphURL(), dialOptions()...)
 	if err != nil {
 		return nil, err
 	}
 	d.conn = conn
 	return d.conn, nil
+}
+
+func dialOptions() []grpc.DialOption {
+	return []grpc.DialOption{
+		grpc.WithInsecure(),
+		unaryClientInterceptor(),
+		streamClientInterceptor(),
+	}
+}
+
+func unaryClientInterceptor() grpc.DialOption {
+	return grpc.WithUnaryInterceptor(
+		grpc_middleware.ChainUnaryClient(
+			grpc_zap.UnaryClientInterceptor(log.Logger(), grpcZapOptions...),
+			grpc_zap.PayloadUnaryClientInterceptor(log.Logger(), defaultDecider),
+		),
+	)
+}
+
+func streamClientInterceptor() grpc.DialOption {
+	return grpc.WithStreamInterceptor(
+		grpc_middleware.ChainStreamClient(
+			grpc_zap.StreamClientInterceptor(log.Logger(), grpcZapOptions...),
+			grpc_zap.PayloadStreamClientInterceptor(log.Logger(), defaultDecider),
+		),
+	)
+}
+
+var defaultDecider = func(context.Context, string) bool { return true }
+var grpcZapOptions = []grpc_zap.Option{
+	grpc_zap.WithLevels(
+		func(codes.Code) zapcore.Level { return zapcore.DebugLevel },
+	),
 }
