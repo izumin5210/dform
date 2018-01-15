8c5aa4b5458dcfa090e7c33628a0e94e8608ecd3
izumin5210 <m@izum.in>
2018-01-15 18:21:34 +0900

    Replace gRPC logger with zap

--- util/log/logger.go
+++ util/log/logger.go
@@ -1,6 +1,11 @@
 package log
 
-import "go.uber.org/zap"
+import (
+	"fmt"
+
+	"go.uber.org/zap"
+	"google.golang.org/grpc/grpclog"
+)
 
 var (
 	logger  *zap.Logger
@@ -15,6 +20,11 @@ func init() {
 func SetLogger(l *zap.Logger) {
 	logger = l
 	sLogger = l.Sugar()
+
+	// See https://github.com/grpc-ecosystem/go-grpc-middleware/blob/d0c54e68681ec7999ac17864470f3bee6521ba2b/logging/zap/grpclogger.go#L13-L18
+	zgl := &zapGrpcLogger{logger.With(zap.String("system", "grpc"), zap.Bool("grpc_log", true))}
+	grpclog.SetLogger(zgl)
+
 	Debug("logger replaced")
 }
 
@@ -56,4 +66,33 @@ func Warn(msg string, args ...interface{}) {
 // Error logs message and key-values pairs in as ERROR
 func Error(msg string, args ...interface{}) {
 	sLogger.Errorw(msg, args...)
+}
+
+// see https://github.com/grpc-ecosystem/go-grpc-middleware/blob/d0c54e68681ec7999ac17864470f3bee6521ba2b/logging/zap/grpclogger.go#L20-L46
+type zapGrpcLogger struct {
+	logger *zap.Logger
+}
+
+func (l *zapGrpcLogger) Fatal(args ...interface{}) {
+	l.logger.Fatal(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLogger) Fatalf(format string, args ...interface{}) {
+	l.logger.Fatal(fmt.Sprintf(format, args...))
+}
+
+func (l *zapGrpcLogger) Fatalln(args ...interface{}) {
+	l.logger.Fatal(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLogger) Print(args ...interface{}) {
+	l.logger.Debug(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLogger) Printf(format string, args ...interface{}) {
+	l.logger.Debug(fmt.Sprintf(format, args...))
+}
+
+func (l *zapGrpcLogger) Println(args ...interface{}) {
+	l.logger.Debug(fmt.Sprint(args...))
 }
