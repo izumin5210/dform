bb65655dbac7a042a693c5c21b6aa064d82aa057
izumin5210 <m@izum.in>
2018-01-18 09:59:18 +0900

    Retrieve ShowSchemaDiffService impl from components on diff command

--- app/cmd/diff.go
+++ app/cmd/diff.go
@@ -6,7 +6,6 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/izumin5210/dform/app/component"
-	"github.com/izumin5210/dform/app/service"
 )
 
 func newDiffCommand(app component.App) *cobra.Command {
@@ -15,13 +14,7 @@ func newDiffCommand(app component.App) *cobra.Command {
 		Short: "Diff schema",
 		Long:  "Diff schema",
 		RunE: func(c *cobra.Command, _ []string) error {
-			svc := service.NewShowSchemaDiffService(
-				app.DgraphSchemaRepository(),
-				app.FileSchemaRepository(),
-				app.UI(),
-			)
-
-			return svc.Perform(context.Background())
+			return app.ShowSchemaDiffService().Perform(context.Background())
 		},
 	}
 }
