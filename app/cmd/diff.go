9deeb06963b41e8fa1f9192db3653d04f9fa68e4
izumin5210 <m@izum.in>
2018-01-22 00:36:17 +0900

    Make diff command confirm and apply schema diff to Dgraph

--- app/cmd/diff.go
+++ app/cmd/diff.go
@@ -6,6 +6,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/izumin5210/dform/app/di"
+	"github.com/izumin5210/dform/util/log"
 )
 
 func newDiffCommand(component di.RootComponent) *cobra.Command {
@@ -14,7 +15,30 @@ func newDiffCommand(component di.RootComponent) *cobra.Command {
 		Short: "Diff schema",
 		Long:  "Diff schema",
 		RunE: func(c *cobra.Command, _ []string) error {
-			return component.ShowSchemaDiffService().Perform(context.Background())
+			var err error
+			ctx := context.Background()
+
+			err = component.ShowSchemaDiffService().Perform(ctx)
+			if err != nil {
+				log.Error("failed to show schema diff", "error", err)
+				return err
+			}
+
+			ok, err := component.UI().Confirm("Would you like to apply it?")
+			if err != nil {
+				log.Error("failed to confirm to apply", "error", err)
+				return err
+			}
+
+			if ok {
+				err = component.ApplySchemaDiffService().Perform(ctx)
+				if err != nil {
+					log.Error("failed to apply schema diff", "error", err)
+					return err
+				}
+			}
+
+			return nil
 		},
 	}
 }
